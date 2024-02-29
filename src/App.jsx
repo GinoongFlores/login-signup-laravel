e7e5512b50600d6fc9d9887e12ca1a5d7b8686f6
ginoongflores <florespaul161@gmail.com>
2024-02-29 10:02:23 +0800

    ref: move `BrowserRouter` main.jsx instead of the app

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,16 @@
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Welcome from "./components/Welcome";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
+
+import { UserAuthProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <UserAuthProvider>
+        <App />
+      </UserAuthProvider>
+    </Router>
   </React.StrictMode>
 );
