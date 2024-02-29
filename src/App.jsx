b8cdd4b493231b605fc33ba6b060df94804ff5a7
ginoongflores <florespaul161@gmail.com>
2024-02-29 15:00:28 +0800

    ref(App): temp protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,41 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import Welcome from "./components/Welcome";
 import { Routes, Route } from "react-router-dom";
+import useAuthContext from "./context/AuthContext";
+import { Navigate, useLocation } from "react-router-dom";
 
 function App() {
+  const { user } = useAuthContext();
+  const location = useLocation();
+
+  const RequireAuth = ({ children }) => {
+    if (!user && location.pathname === "/") {
+      return <Navigate to={"/login"} replace />;
+    } else if (user && location.pathname === "/login") {
+      return <Navigate to={"/"} replace />;
+    } else {
+      return children;
+    }
+  };
   return (
     <>
       <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <Welcome />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RequireAuth>
+              <Login />
+            </RequireAuth>
+          }
+        />
         <Route path="/register" element={<Register />} />
       </Routes>
     </>
