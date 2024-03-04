0e0506883e0f1c54facd5e9ce0d49104de873995
ginoongflores <florespaul161@gmail.com>
2024-03-04 13:18:29 +0800

    ref: temp add features on Register and Welcome

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import useAuthContext from "../context/AuthContext";
 import { useFormik } from "formik";
 import * as Yup from "yup";
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,33 +1,18 @@
-import { Link } from "react-router-dom";
-import useAuthContext from "../context/AuthContext";
-import { useEffect } from "react";
+import Navbar from "./Navbar";
 
 const Welcome = () => {
-  const { user, getUser } = useAuthContext();
-  // console.log(user.data?.name);
-
-  useEffect(() => {
-    if (!user) {
-      getUser();
-    }
-  }, [getUser, user]);
-
   return (
     <>
-      <section className="bg-gray-900 dark:text-white">
-        <div className="flex justify-around items-center">
+      <Navbar />
+      <section>
+        <div className="max-w-sm p-6 rounded-lg bg-white">
           <div>
-            <h1 className="text-3xl">Welcome! {user?.name.toUpperCase()}</h1>
-          </div>
-
-          <div>
-            <ul className="flex gap-8">
-              <li>Logout</li>
-              {/* <li>Login</li> */}
-              <li>
-                <Link to="/register">Register</Link>
-              </li>
-            </ul>
+            <h2 className="text-2xl">
+              Name: <span></span>
+            </h2>
+            <h2 className="text-2xl">
+              Email: <span></span>
+            </h2>
           </div>
         </div>
       </section>
